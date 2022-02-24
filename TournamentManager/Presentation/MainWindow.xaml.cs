1c513d46b6a3ac3245b294725ce73479c69a23bd
Josh Kempner <joshkempner@users.noreply.github.com>
2022-02-24 16:27:32 -0500

    Overlay on main window

--- TournamentManager/Presentation/MainWindow.xaml.cs
+++ TournamentManager/Presentation/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Reactive.Disposables;
+﻿using System;
+using System.Reactive.Disposables;
+using System.Windows;
 using ReactiveUI;
 
 namespace TournamentManager.Presentation
@@ -18,7 +20,14 @@ namespace TournamentManager.Presentation
                     .DisposeWith(disposables);
                 this.OneWayBind(ViewModel, vm => vm.TournamentVM, v => v.MainTournamentsView.ViewModel)
                     .DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.OverlayVM, v => v.OverlayViewHost.ViewModel)
+                    .DisposeWith(disposables);
             });
+
+#pragma warning disable CS8602
+            this.WhenAnyValue(x => x.ViewModel.OverlayVM)
+                .Subscribe(x => Overlay.Visibility = x is null ? Visibility.Collapsed : Visibility.Visible);
+#pragma warning restore CS8602
         }
     }
 }
